a1444d6514935966fa8456fb30db56275ce04cdf
theprogressapp <34925940+theprogressapp@users.noreply.github.com>
2017-12-28 19:18:30 -0500

    First commit

--- app/src/main/java/net/theprogressapp/progressproject9/MainActivity.java
+++ app/src/main/java/net/theprogressapp/progressproject9/MainActivity.java
@@ -2,8 +2,19 @@ package net.theprogressapp.progressproject9;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void clickFunction (View view) {
+
+        EditText somethingEditText = (EditText) findViewById(R.id.somethingEditText);
+
+        Toast.makeText(MainActivity.this, "You typed in " + somethingEditText.getText().toString(), Toast.LENGTH_SHORT).show();
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
